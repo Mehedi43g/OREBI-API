a138721e96c11a7ee69871c6d2509ae79bbadffa
HRK439 <xdemyyt@gmail.com>
2025-07-27 01:43:02 +0600

    super

---
+++ src/Components/BigTime.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from './Container'
+
+const BigTime = () => {
+  return (
+    <section>
+        <Container>
+            <div className="">
+                <p>hallow</p>
+            </div>
+        </Container>
+    </section>
+  )
+}
+
+export default BigTime
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import Banner from "../Components/Banner"
+import Banntw from "../Components/Banntw"
+import BanThree from "../Components/BanThree"
+import BigTime from "../Components/BigTime"
+import Header from "../Components/Header"
+import Nav from "../Components/Nav"
+ 
+
+ 
+const Home = () => {
+     
+    return (
+        <>
+        <Header/>
+        <Nav/>
+        <Banner/>
+        <Banntw/>
+        <BanThree/>
+        <BigTime/>
+
+        </>
+    )
+}
+
+export default Home
